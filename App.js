71cd9aea21623ee2df2caf9e19f378f71bff5e37
Damiandesign93 <112899588+Damiandesign93@users.noreply.github.com>
2023-05-01 22:08:40 -0300

    mi primer commit

--- App.js
+++ App.js
@@ -1,11 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+import Modal from "./src/components/modal";
+import { useState } from "react";
 
 export default function App() {
+  const [textItem, setTextItem] = useState("");
+  const [list, setList] = useState([]);
+  const [itemSelected, setItemSelected] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const onHandleChangeText = text => {
+    setTextItem(text);
+    console.log(text);
+  };
+
+  const addItem = () => {
+    console.log("aqui agregamos el item", textItem);
+    setList(prevState => [
+      ...prevState,
+      { name: textItem, id: Math.random().toString() },
+    ]);
+    setTextItem("");
+  };
+
+  const onHandleModal = item => {
+    console.log("en esta funcion seteo el item y abro el modal");
+    setItemSelected(item);
+    setModalVisible(true);
+  };
+
+  const onHandleDelete = item => {
+    console.log("eliminar este item", item);
+    setList(prevState =>
+      prevState.filter(element => element.name !== item.name)
+    );
+    setModalVisible(false);
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.renderItemStyle}>
+      <Text>{item.name}</Text>
+      <Button title="X" onPress={() => onHandleModal(item)} color={"red"} />
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <Text style={styles.titleContainer}>EcoVentas</Text>
+        <View style={styles.addItemContainer}>
+          <TextInput
+            placeholder="elemento de la lista"
+            style={styles.input}
+            onChangeText={onHandleChangeText}
+            value={textItem}
+          />
+          <Button title="Presiona aqui" onPress={addItem} />
+        </View>
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
+      <Modal
+        isVisible={modalVisible}
+        actionDeleteItem={() => onHandleDelete(itemSelected)}
+        itemSelected={itemSelected}
+      />
     </View>
   );
 }
@@ -13,8 +84,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F8F8F8",
+  },
+  inputContainer: {
+    height: 200,
+    paddingHorizontal: 30,
+    paddingTop: 40,
+  },
+  titleContainer: {
+    paddingTop: 30,
+    marginLeft: 20,
+    marginBottom: 20,
+    fontSize: 40,
+    fontWeight: "500",
+    color: "#1E283C",
+  },
+  addItemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderBottomColor: "green",
+    borderBottomWidth: 2,
+    width: 200,
+  },
+  listContainer: {
+    flex: 2,
+    marginHorizontal: 30,
+    marginTop: 20,
+    padding: 3,
+  },
+  renderItemStyle: {
+    height: 60,
+    flexDirection: "row",
+    marginBottom: 25,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 10,
+    justifyContent: "space-around",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 2,
+    elevation: 4,
   },
 });
